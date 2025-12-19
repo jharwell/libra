2671763fcbff089756360ab2c2fc16a0e87bf2ed
John Harwell <john.r.harwell@gmail.com>
2025-12-19 11:48:09 -0600

    Tweak clang-tidy configuration

--- cmake/libra/version.cmake
+++ cmake/libra/version.cmake
@@ -6,4 +6,4 @@
 # The current version of LIBRA, to make debugging strange build problems easier.
 # This is set in CI; the value present in git might not match the conan package
 # version.
-set(LIBRA_VERSION 0.9.11)
+set(LIBRA_VERSION 0.9.12)
